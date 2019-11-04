ca2c70f81905770284449a9159ba9d06ef54a291
Danang Galuh Tegar Prasetyo <danang.galuh.t@gmail.com>
2019-11-04 15:04:36 +0700

    Initial: React native

--- native/ios/Pods/Target Support Files/Pods-mycashier/Pods-mycashier-dummy.m
+++ native/ios/Pods/Target Support Files/Pods-mycashier/Pods-mycashier-dummy.m
@@ -1,5 +1,5 @@
 #import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_mycashier : NSObject
+@interface PodsDummy_Pods_myCashier : NSObject
 @end
-@implementation PodsDummy_Pods_mycashier
+@implementation PodsDummy_Pods_myCashier
 @end
